4322088c06d5558788cbd30849df78e28154238a
ClemNTTS <karmaclem@gmail.com>
2024-03-02 23:28:29 +0100

    all

---
+++ quad.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	QuadA(10, 5)
+}
+
+func QuadA(x, y int) {
+	for row := 0; row < y; row++ {
+		for col := 0; col < x; col++ {
+			//Coins
+			if (col == 0 || col == x-1) && (row == 0 || row == y-1) {
+				print(0)
+
+				//lignes Haut et Bas
+			} else if (col != x-1 || col != 0) && (row == 0 || row == y-1) {
+				print("-")
+
+				//Bords
+			} else if (col == 0 || col == x-1) && (row != 0 || row != y-1) {
+				print("|")
+
+				//Centre
+			} else {
+				print(" ")
+			}
+		}
+		print("\n")
+	}
+}
---
+++ split.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fonction("HelloHAjeHAsuiHAclement", "HA"))
+}
+
+func fonction(s string, sep string) []string {
+	//création d'un tableau
+	tbl := []string(nil)
+	startindex := 0
+	Lsep := len(sep)
+
+	//boucle jusqu'a len - len du sep pour eviter runtime
+	for i := 0; i < len(s)-Lsep+1; i++ {
+
+		//BANGER
+		if s[i:i+Lsep] == sep {
+			//CLASSICO
+			tbl = append(tbl, s[startindex:i])
+			startindex = i + Lsep
+			i = i + Lsep - 1
+		}
+	}
+	tbl = append(tbl, s[startindex:])
+	return tbl
+}
+
+/*
+	s = "HelloHAjeHAsuiHAclement"
+	sep = "HA"
+	return = {"Hello", "je", "suis", "clement"}
+*/
